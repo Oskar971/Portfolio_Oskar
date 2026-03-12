7ff0ab8fc30af536d17481a6986b088ab15013d0
Oskar971 <ossta007@osloskolen.no>
2026-03-12 10:49:42 +0100

    finn pussing og header bar
    link W a pluss

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-document.querySelector(".about-link").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  document.querySelector("#about").scrollIntoView({
-    behavior: "smooth",
-  });
-});
-
 const knapp = document.getElementById("toggleProsjekter");
 const prosjektContainer = document.getElementById("prosjekt20");
 
 knapp.addEventListener("click", () => {
-  prosjektContainer.classList.toggle("hidden"); // legger til/fjerner hidden
+  prosjektContainer.classList.toggle("hidden");
 
   if (prosjektContainer.classList.contains("hidden")) {
     knapp.textContent = "Vis prosjekter";
   } else {
     knapp.textContent = "Skjul prosjekter";
   }
+});
+
+document.querySelector(".about-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  document.querySelector("#about").scrollIntoView({
+    behavior: "smooth",
+  });
 });
